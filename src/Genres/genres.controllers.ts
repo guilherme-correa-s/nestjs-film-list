b4a24aa13abf6a3d1c279a8573f3ec4337826be7
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2021-10-02 14:34:37 -0300

    feat(Genres): add method POST

--- src/Genres/genres.controllers.ts
+++ src/Genres/genres.controllers.ts
@@ -1,5 +1,19 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateGenreDTO } from './dto/CreateGenreDTO.dto';
+import { UpdateGenreDTO } from './dto/UpdateGenreDTO.dto';
 import { GenresService } from './genres.service';
 
 @Controller('genres')
@@ -14,5 +28,21 @@ export class GenresController {
   @Post()
   async create(@Body() data: CreateGenreDTO) {
     return this.genresService.create(data);
+  }
+
+  @Put('/:id')
+  @UsePipes(ValidationPipe)
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() data: UpdateGenreDTO,
+  ) {
+    return this.genresService.update(id, data);
+  }
+
+  @Delete('/:id')
+  @UsePipes(ValidationPipe)
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    return this.genresService.delete(id);
   }
 }
--- src/Genres/genres.service.ts
+++ src/Genres/genres.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateGenreDTO } from './dto/CreateGenreDTO.dto';
+import { UpdateGenreDTO } from './dto/UpdateGenreDTO.dto';
 
 @Injectable()
 export class GenresService {
@@ -12,5 +13,13 @@ export class GenresService {
 
   async create(data: CreateGenreDTO) {
     return await this.prismaService.genre.create({ data });
+  }
+
+  async update(id: number, data: UpdateGenreDTO) {
+    return await this.prismaService.genre.update({ data, where: { id } });
+  }
+
+  async delete(id: number) {
+    return await this.prismaService.genre.delete({ where: { id } });
   }
 }
