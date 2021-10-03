75a33365df9b37c8b6c5c24b856fea2130b1b39b
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2021-10-03 12:16:34 -0300

    feat(Genres): add Property Decorator

--- src/Genres/dto/CreateGenreDTO.dto.ts
+++ src/Genres/dto/CreateGenreDTO.dto.ts
@@ -1,6 +1,7 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateGenreDTO {
   @IsNotEmpty()
+  @IsString()
   name: string;
 }
