9ee63fefa8d29c9870096dfbb5e91759548ab083
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2021-10-03 12:14:49 -0300

    feat(Films): add Property Decorator

--- src/Films/dto/CreateFilm.dto.ts
+++ src/Films/dto/CreateFilm.dto.ts
@@ -1,12 +1,16 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateFilmDTO {
   @IsNotEmpty()
+  @IsString()
   name: string;
   @IsOptional()
+  @IsString()
   imagem?: string;
   @IsNotEmpty()
+  @IsString()
   release_date: string;
   @IsNotEmpty()
+  @IsString()
   duration: string;
 }
